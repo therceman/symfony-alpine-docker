1b6cc3716d5667c37a5ff42dc2f17fcf0a73a929
RCEman <theonekilaz@gmail.com>
2022-02-16 15:32:10 +0200

    Docker logs support added: docker logs php

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-ln -sf /dev/stdout /var/log/apache2/access.log && \
-ln -sf /dev/stderr /var/log/apache2/error.log
-
-mkdir -p /run/apache2 && \
-  /usr/sbin/httpd -D FOREGROUND
+mkdir -p /run/apache2 && /usr/sbin/httpd -D FOREGROUND
 
 exit "$@"
